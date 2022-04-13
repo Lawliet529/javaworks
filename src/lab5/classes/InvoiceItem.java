e52630182ea27a055b09543bcd223d63b652aa2f
Lawliet529 <datnguyen7112002@gmail.com>
2022-04-13 14:50:11 +0700

    Add InvoiceItem.java

---
+++ src/lab5/classes/InvoiceItem.java
@@ -0,0 +1,68 @@
+package lab5.classes;
+
+/**
+ * 1. Exercises on Classes
+ *
+ * <p>1.5 The InvoiceItem Class
+ *
+ * <p>Class InvoiceItem models an item of an invoice, with ID, description, quantity and unit
+ * price.
+ */
+public class InvoiceItem {
+
+  private String id;
+  private String desc;
+  private int qty;
+  private double unitPrice;
+
+  /**
+   * @param id        ID.
+   * @param desc      description.
+   * @param qty       quantity.
+   * @param unitPrice unit price.
+   */
+  public InvoiceItem(String id, String desc, int qty, double unitPrice) {
+    this.id = id;
+    this.desc = desc;
+    this.qty = qty;
+    this.unitPrice = unitPrice;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getDesc() {
+    return desc;
+  }
+
+  public int getQty() {
+    return qty;
+  }
+
+  public void setQty(int qty) {
+    this.qty = qty;
+  }
+
+  public double getUnitPrice() {
+    return unitPrice;
+  }
+
+  public void setUnitPrice(double unitPrice) {
+    this.unitPrice = unitPrice;
+  }
+
+  public double getTotal() {
+    return unitPrice * qty;
+  }
+
+  @Override
+  public String toString() {
+    return "InvoiceItem{" +
+        "id='" + id + '\'' +
+        ", desc='" + desc + '\'' +
+        ", qty=" + qty +
+        ", unitPrice=" + unitPrice +
+        '}';
+  }
+}
