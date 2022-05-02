c06a7e977736759c6c30213e58eb009d23198d93
Lawliet529 <datnguyen7112002@gmail.com>
2022-05-02 21:20:10 +0700

    1.6 Abstract Superclass Animal and its Implementation Subclasses

---
+++ src/lab8/polymorphism/animal/Animal.java
@@ -0,0 +1,12 @@
+package lab8.polymorphism.animal;
+
+public abstract class Animal {
+
+  private String name;
+
+  public Animal(String name) {
+    this.name = name;
+  }
+
+  public abstract void greets();
+}
